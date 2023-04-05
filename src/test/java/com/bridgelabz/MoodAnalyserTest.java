f7d69689602059f95d750879b14f629d497068d7
naveenkumargorakala <nicenaveengorakala@gmail.com>
2023-04-05 12:21:38 +0530

    Added: Null Mood Should Return Happy

---
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,36 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyserTest {
+
+    static MoodAnalyser moodAnalyser;
+
+    @BeforeAll
+    public static void init() throws MoodAnalysisException {
+        moodAnalyser = new MoodAnalyser();
+        System.out.println("Implement to all");
+    }
+
+    @Test
+    void givenMessageWhenCheckedReturnSad() {
+        try {
+            String mood = moodAnalyser.analyseMood();
+            Assertions.assertEquals("SAD", mood);
+        } catch (MoodAnalysisException e) {
+            System.out.println("Happy");
+        }
+    }
+
+    @Test
+    void givenMessageWhenCheckedReturnHappy() {
+        try {
+            String mood = moodAnalyser.analyseMood( );
+            Assertions.assertEquals("Happy", mood);
+        } catch (MoodAnalysisException e) {
+            System.out.println("Happy");
+        }
+    }
+}

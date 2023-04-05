48f3e104ea18422b59bda1cd4822bdd917792dbe
naveenkumargorakala <nicenaveengorakala@gmail.com>
2023-04-05 12:27:44 +0530

    Added: Add Custom Exception

--- src/main/java/com/bridgelabz/MoodAnalysisException.java
+++ src/main/java/com/bridgelabz/MoodAnalysisException.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-public class MoodAnalysisException extends Exception{
+public class MoodAnalysisException extends Exception {
     public MoodAnalysisException(String message ) {
         super(message);
     }
